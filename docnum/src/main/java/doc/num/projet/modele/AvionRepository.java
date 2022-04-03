1845c829b604fe808d4ec31b0e11e27bff06d90e
Elias Romdan <elyesbr@hotmail.fr>
2022-04-03 23:32:40 +0200

    Update Project

---
+++ docnum/src/main/java/doc/num/projet/modele/AvionRepository.java
@@ -0,0 +1,11 @@
+package doc.num.projet.modele;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * 
+ * @author Nicolas T.
+ */
+public interface AvionRepository extends CrudRepository<Avion, Long> {
+
+}
